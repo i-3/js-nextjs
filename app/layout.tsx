8b3677b96639a172837809f9487cbf2c8771d342
i-3 <31x1962@gmail.com>
2026-06-21 16:45:10 +0300

    docker network setup & dark theme via script

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,22 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark');
+                } else {
+                  document.documentElement.classList.remove('dark');
+                }
+              } catch (_) {}
+            `,
+          }}
+        />
+      </head>
+
       <body className={`${inter.className} antialiased overflow-x-hidden`}>
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
